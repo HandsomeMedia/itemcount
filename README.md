8961ed25472c6eb3e1eec331fc12694541e4df53
Amrir Toufali <Amrir@HandsomeMedia.com>
2017-06-26 11:36:56 -0700

    Meta Cleanup

---
+++ README.md
@@ -0,0 +1,23 @@
+To Run:
+open root directory in console and type 'node index.js' or 'npm start'
+
+===========
+
+Project Requirements:
+- allow user to supply a CLI argument containing a comma-separated list
+  - if no argument is given, load `tags.txt`
+- count how many times each list item appears within the objects in `data/*.json` (_note:_ objects can be nested).
+- final output should be formatted like this (sorted by most popular tag first):
+
+```
+pizza     15
+spoon     2
+umbrella  0
+cats      0
+```
+
+- cache the result so that subsequent runs can return the result immediately without needing to process the files.
+- use only core modules
+- use asynchronous variants of the file IO functions (eg. use `fs.readFile` not `fs.readFileSync`).
+- if any of the data files contain invalid JSON, log the error with `console.error` and continue, ignoring that file.
+- use latest version of node (at time of writing), but use plain callbacks instead of promises.
--- memoize.js
+++ memoize.js
@@ -1,36 +1,11 @@
 /*eslint no-console: 0*/
 
 /*
- * memoize.js
- * by @philogb and @addyosmani
- * with further optimizations by @mathias
- * and @DmitryBaranovsk
- * perf tests: http://bit.ly/q3zpG3
- * Released under an MIT license.
- */
-
- /*
-function memoize(fn) {
-  return function() {
-    var args = Array.prototype.slice.call(arguments),
-      hash = '',
-      i = args.length;
-    currentArg = null;
-    while (i--) {
-      currentArg = args[i];
-      hash += (currentArg === Object(currentArg)) ?
-        JSON.stringify(currentArg) : currentArg;
-      fn.memoize || (fn.memoize = {});
-    }
-    return (hash in fn.memoize) ? fn.memoize[hash] :
-      fn.memoize[hash] = fn.apply(this, args);
-  };
-}
+ * Memoize function is optized for simplicity not performance.
+ * See here for more advanced implementations:
+ * https://addyosmani.com/blog/faster-javascript-memoization/
 */
 
-/*
- *https://medium.com/front-end-hacking/today-i-learned-memoization-with-pure-functions-in-es6-33a4765518b5
- */
 module.exports = fn => {
   const cache = {};
   return (...args) => {
